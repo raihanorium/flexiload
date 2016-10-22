5b46d2c10f799fa002911ce7c47daadd13f7bab8
Raihan <raihan.dk@gmail.com>
2016-10-22 13:49:01 +0600

    Logged in user name in navbar

--- navbar.php
+++ navbar.php
@@ -12,7 +12,7 @@
 		<?php if(isset($_SESSION['LOGGED_IN_USER'])): ?>
 		<ul class="nav navbar-nav navbar-right">
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> User <b class="caret"></b></a>
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?php echo $_SESSION['LOGGED_IN_USER']['full_name']; ?> <b class="caret"></b></a>
 				<ul class="dropdown-menu">
 					<li><a href="?p=profile">Profile</a></li>
 					<li><a href="?p=logout">Logout</a></li>
